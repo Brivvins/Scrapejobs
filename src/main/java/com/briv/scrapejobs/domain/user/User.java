55f48eb088c8dccb114f1fd4eea3b6bc566cbd93
Brivvins <markbrivvins@gmail.com>
2025-12-29 10:49:24 +0300

    Create User entity and repository

---
+++ src/main/java/com/briv/scrapejobs/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.briv.scrapejobs.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email")
+        })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;   // Remember to hash
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/briv/scrapejobs/repository/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.briv.scrapejobs.repository.user;
+
+import com.briv.scrapejobs.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
