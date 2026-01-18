bc29511b7d6b56ddf7b00d0706eadec67555f09f
Brivvins <markbrivvins@gmail.com>
2026-01-18 03:26:02 +0300

    Created custom exception wrapper

---
+++ src/main/java/com/briv/scrapejobs/exception/ScrapingFailedException.java
@@ -0,0 +1,8 @@
+package com.briv.scrapejobs.exception;
+
+public class ScrapingFailedException extends RuntimeException {
+
+    public ScrapingFailedException(Throwable cause) {
+        super(cause);
+    }
+}
